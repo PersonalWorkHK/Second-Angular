1883aab450286b63611de1f3f0c5c2d4f03fa2ec
Harsh <hk931017@gmail.com>
2024-12-01 20:33:53 +0530

    section 2 header and user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
   title = 'first-angular-project';
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-user';
+
+const RandomIndex=Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+ Users=DUMMY_USERS[RandomIndex];
+
+ get imagePath(){
+  return 'assets/users/'+this.Users.avatar
+ }
+}
